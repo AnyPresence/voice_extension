0660d9dd301c4a32cbc2037cf2e36fca7a23cb70
Khem Veasna <kveasna@anypresence.com>
2012-02-13 12:21:56 -0500

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VoiceExtension::Application.config.secret_token = 'b6824604555dba4aaacc516a09396adc051203170159b61ed5413e1294178f898880f5a685c63ea432879edcbf00b1818bda57ed59719f95996086c8e569e91d'
