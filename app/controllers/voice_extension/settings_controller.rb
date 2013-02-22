f8afc95a1e8e5091d813093c4c985c625bfa0379
Khem Veasna <kveasna@anypresence.com>
2013-02-22 14:17:27 -0500

    Set twilio voice url when configuring the extension.

--- app/controllers/voice_extension/settings_controller.rb
+++ app/controllers/voice_extension/settings_controller.rb
@@ -5,20 +5,20 @@ module VoiceExtension
     before_filter :authenticate_admin!
     
     def index
-      @twilio_phone_number = AP::VoiceExtension::Voice::Config.instance.configuration[:twilio_phone_number] || ""
+      @consume_phone_number = AP::VoiceExtension::Voice::Config.instance.configuration[:consume_phone_number] || ""
       @consume_url = AP::VoiceExtension::Voice::Config.instance.configuration[:consume_url] || ""
     end
     
     def update
-      twilio_phone_number = params[:twilio_phone_number]
+      consume_phone_number = params[:consume_phone_number]
       consume_url = params[:consume_url]
       
-      AP::VoiceExtension::Voice::Config.instance.configuration.merge!(twilio_phone_number: twilio_phone_number, consume_url: consume_url)
+      AP::VoiceExtension::Voice::Config.instance.configuration.merge!(consume_phone_number: consume_phone_number, consume_url: consume_url)
       
       # Try to set the consume url for the phone number
       begin 
         twilio_wrapper = VoiceExtension::TwilioVoiceWrapper::Voice.new
-        twilio_wrapper.update_voice_url(twilio_phone_number, consume_url)
+        twilio_wrapper.update_voice_url(consume_phone_number, consume_url)
       rescue
         error = "Unable to setup the twilio phone number/url: #{$!.message}"
         Rails.logger.error(error)
--- lib/ap/voice_extension/voice.rb
+++ lib/ap/voice_extension/voice.rb
@@ -5,7 +5,8 @@ module AP
     module Voice
       def self.config_account(config={})
         Config.instance.configuration ||= HashWithIndifferentAccess.new
-        Config.instance.configuration = Config.instance.configuration.merge(config)        
+        Config.instance.configuration = Config.instance.configuration.merge(config)
+        AP::VoiceExtension::Voice::Config.instance.configuration[:consume_url] = ENV['AP_IVR_NOTIFIER_CONSUME_URL']   
       end
 
       class Config
